e654b8e0c380a5631ece82fb7f6365bd4ddf6ea9
Kacper Budny <kacperbudny98@gmail.com>
2022-09-28 22:02:39 +0200

    Style not found page

--- client/src/components/layout/NotFound/NotFound.js
+++ client/src/components/layout/NotFound/NotFound.js
@@ -4,10 +4,14 @@ import styles from "./NotFound.module.scss";
 
 const NotFound = () => {
   return (
-    <div className={styles.container}>
-      <h1>Oops!</h1>
-      <p>We could not find what you're looking for.</p>
-      <Link to="/">Take me back to the home page</Link>
+    <div className={styles.centeringContainer}>
+      <div className={styles.container}>
+        <h1>Oops!</h1>
+        <p className={styles.paragraph}>
+          We could not find what you're looking for.
+        </p>
+        <Link to="/">Take me back to the home page</Link>
+      </div>
     </div>
   );
 };
