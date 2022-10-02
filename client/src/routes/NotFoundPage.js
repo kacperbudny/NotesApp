55e57a7aa25ef12e84420d267bbe6b112c523a55
Kacper Budny <kacperbudny98@gmail.com>
2022-10-02 20:18:00 +0200

    Bring code back to NotFoundPage since it no longer has its own stylesheet

--- client/src/routes/NotFoundPage.js
+++ client/src/routes/NotFoundPage.js
@@ -1,8 +1,15 @@
+import CenteredContainer from "@components/common/CenteredContainer/CenteredContainer";
 import React from "react";
-import NotFound from "@components/layout/NotFound";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
-  return <NotFound />;
+  return (
+    <CenteredContainer>
+      <h1>Oops!</h1>
+      <p>We could not find what you're looking for.</p>
+      <Link to="/">Take me back to the home page</Link>
+    </CenteredContainer>
+  );
 };
 
 export default NotFoundPage;
