8cb7b1e030306a3b15025ddac4acbd233163cfb9
Kacper Budny <kacperbudny98@gmail.com>
2022-10-19 21:02:43 +0200

    Style sidebar

--- client/src/components/layout/Sidebar/Sidebar.js
+++ client/src/components/layout/Sidebar/Sidebar.js
@@ -1,12 +1,24 @@
 import React from "react";
 import SidebarLink from "@components/layout/SidebarLink";
 import styles from "./Sidebar.module.scss";
+import { faNoteSticky } from "@fortawesome/free-regular-svg-icons";
+import { faInbox } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   return (
     <nav className={styles.container}>
-      <SidebarLink to="/">Notes</SidebarLink>
-      <SidebarLink to="/archive">Archive</SidebarLink>
+      <ul>
+        <li>
+          <SidebarLink icon={faNoteSticky} to="/">
+            Notes
+          </SidebarLink>
+        </li>
+        <li>
+          <SidebarLink icon={faInbox} to="/archive">
+            Archive
+          </SidebarLink>
+        </li>
+      </ul>
     </nav>
   );
 };
--- client/src/components/layout/SidebarLink/SidebarLink.js
+++ client/src/components/layout/SidebarLink/SidebarLink.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
-import { faNoteSticky } from "@fortawesome/free-regular-svg-icons";
+import styles from "./SidebarLink.module.scss";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SidebarLink = ({ children, icon, to }) => {
   return (
     <NavLink to={to}>
-      <FontAwesomeIcon icon={faNoteSticky} /> {children}
+      {({ isActive }) => (
+        <div className={`${styles.link} ${isActive && styles.activeLink}`}>
+          <FontAwesomeIcon icon={icon} size="lg" className={styles.icon} />
+          <span>{children}</span>
+        </div>
+      )}
     </NavLink>
   );
 };
 
 SidebarLink.propTypes = {
-  icon: PropTypes.node.isRequired,
+  icon: PropTypes.object.isRequired,
   to: PropTypes.string.isRequired,
 };
 
